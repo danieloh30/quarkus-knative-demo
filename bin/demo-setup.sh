fbafd8252e9b50af97562eb6ff7b4bdb48844193
Daniel Oh <doh@danieloh.redhat.com>
2019-05-05 23:02:14 -0400

    Add minishift-setup script

--- bin/demo-setup.sh
+++ bin/demo-setup.sh
@@ -5,9 +5,10 @@ eval $(minishift docker-env)
 
 oc login $(minishift ip):8443 -u admin -p admin
 
-oc delete project  quarkus-knative
+oc delete project quarkus-knative
 
 oc new-project quarkus-knative --display-name='Quarkus on Knative Demo'
 oc adm policy add-scc-to-user privileged -z default 
 oc adm policy add-scc-to-user anyuid -z default
+oc label namespace quarkus-knative istio-injection=enabled
 
--- bin/install-istio-knative.sh
+++ bin/install-istio-knative.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ## Istio
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio-crds.yaml && \
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/istio.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.1/istio-crds.yaml && \
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.1/istio.yaml
 
 #Knative-serving
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.4.0/serving.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.5.1/serving.yaml
 
 #Knative-build
-kubectl apply --filename https://github.com/knative/build/releases/download/v0.4.0/build.yaml
+kubectl apply --filename https://github.com/knative/build/releases/download/v0.5.0/build.yaml
 
 #Knative-eventing
-kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.4.0/release.yaml && \
-kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.4.0/release.yaml && \
-kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.4.0/third_party/config/build/clusterrole.yaml
+kubectl apply --filename https://github.com/knative/eventing/releases/download/v0.5.0/release.yaml && \
+kubectl apply --filename https://github.com/knative/eventing-sources/releases/download/v0.5.0/release.yaml && \
+kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.5.0/third_party/config/build/clusterrole.yaml
